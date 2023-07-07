1105bc08bb326029717aed0aa5f8971c72f9decc
RomiRao <raoromina96@gmail.com>
2023-07-06 22:43:02 -0300

    bringing elements to js and restructuring settings panel

---
+++ script/script.js
@@ -0,0 +1,48 @@
+
+//Canvas 
+const canvas = document.getElementById('canvas');
+
+//Chopper parts
+const chopperHead = document.getElementById('chopper-head');
+const chopperBody = document.getElementById('chopper-body');
+const chopperLegs = document.getElementById('chopper-legs');
+const chopperEyes = document.getElementById('chopper-eyes');
+const chopperMouth = document.getElementById('chopper-mouth');
+const chopperPants = document.getElementById('chopper-pants');
+const chopperObject = document.getElementById('chopper-object');
+
+//Settings buttons 
+//Head
+const headLabel = document.getElementById('head-label');
+const rightHead = document.getElementById('right-head');
+const leftHead = document.getElementById('left-head');
+
+//Body
+const bodyLabel = document.getElementById('body-label');
+const rightBody = document.getElementById('right-body');
+const leftBody = document.getElementById('left-body');
+
+//Mouth
+const mouthLabel = document.getElementById('mouth-label');
+const rightMouth = document.getElementById('right-mouth');
+const leftMouth = document.getElementById('left-mouth');
+
+//Eyes
+const eyesLabel = document.getElementById('eyes-label');
+const rightEyes = document.getElementById('right-eyes');
+const leftEyes = document.getElementById('left-eyes');
+
+//Pants
+const pantsLabel = document.getElementById('pants-label');
+const rightPants = document.getElementById('right-pants');
+const leftPants = document.getElementById('left-pants');
+
+//Legs
+const legsLabel = document.getElementById('legs-label');
+const rightLegs = document.getElementById('right-legs');
+const leftLegs = document.getElementById('left-legs');
+
+//Object
+const objectLabel = document.getElementById('object-label');
+const rightObject = document.getElementById('right-object');
+const leftObject = document.getElementById('left-object');
