056aa30d9c7d2fea96e03f7d7b25f1656eab61bd
RomiRao <raoromina96@gmail.com>
2024-02-12 21:16:30 -0300

    changing selectors

--- script/script.js
+++ script/script.js
@@ -1,27 +1,16 @@
-//Canvas
-const chopperContainer = document.getElementById("chopper-container");
-
-//BODY PARTS
-const chopperHead = document.getElementById("chopper-head");
-const chopperBody = document.getElementById("chopper-body");
-const chopperLegs = document.getElementById("chopper-legs");
-const chopperEyes = document.getElementById("chopper-eyes");
-const chopperMouth = document.getElementById("chopper-mouth");
-const chopperPants = document.getElementById("chopper-pants");
-const chopperObject = document.getElementById("chopper-object");
+const $ = (selector) => document.getElementById(selector);
 
 //--------
 //DOWNLOAD BUTTON
 //-------
-const saveButton = document.getElementById("save-button");
-saveButton.addEventListener("click", () => downloadChopper());
+$("save-button").addEventListener("click", () => downloadChopper());
 
 const downloadChopper = () => {
     let scale = 8;
     domtoimage
-        .toBlob(chopperContainer, {
-            width: chopperContainer.clientWidth * scale,
-            height: chopperContainer.clientHeight * scale,
+        .toBlob($("chopper-container"), {
+            width: $("chopper-container").clientWidth * scale,
+            height: $("chopper-container").clientHeight * scale,
             style: {
                 transform: "scale(" + scale + ")",
                 transformOrigin: "top left",
@@ -78,7 +67,7 @@ preload_image("../choppify/style/body_parts/Zoro/zoro_object.png");
 preload_image("../choppify/style/body_parts/Zoro/zoro_legs.png");
 
 //---------
-//Character gio es un gil
+//Characters
 //--------
 
 const character = [
@@ -130,220 +119,253 @@ const characterAmount = Object.keys(character).length;
 //SETINGS BUTTONS
 //--------------
 
+//Left function
+
 //HEAD --------
-
-const headLabel = document.getElementById("head-label");
-const nextHead = document.getElementById("right-head");
-const previousHead = document.getElementById("left-head");
-
 let iHead = 0;
 
-previousHead.addEventListener("click", () => {
+$("left-head").addEventListener("click", () => {
     iHead--;
     if (iHead >= 0) {
-        headLabel.innerText = character[iHead].name;
-        chopperHead.style.backgroundImage = `url('${character[iHead].head}')`;
+        $("head-label").innerText = character[iHead].name;
+        $(
+            "chopper-head"
+        ).style.backgroundImage = `url('${character[iHead].head}')`;
     } else {
         iHead = characterAmount - 1;
-        headLabel.innerText = character[iHead].name;
-        chopperHead.style.backgroundImage = `url('${character[iHead].head}')`;
+        $("head-label").innerText = character[iHead].name;
+        $(
+            "chopper-head"
+        ).style.backgroundImage = `url('${character[iHead].head}')`;
     }
 });
 
-nextHead.addEventListener("click", () => {
+$("right-head").addEventListener("click", () => {
     iHead++;
     if (iHead < characterAmount) {
-        headLabel.innerText = character[iHead].name;
-        chopperHead.style.backgroundImage = `url('${character[iHead].head}')`;
+        $("head-label").innerText = character[iHead].name;
+        $(
+            "chopper-head"
+        ).style.backgroundImage = `url('${character[iHead].head}')`;
     } else {
         iHead = 0;
-        headLabel.innerText = character[iHead].name;
-        chopperHead.style.backgroundImage = `url('${character[iHead].head}')`;
+        $("head-label").innerText = character[iHead].name;
+        $(
+            "chopper-head"
+        ).style.backgroundImage = `url('${character[iHead].head}')`;
     }
 });
 
 //BODY ------
-const bodyLabel = document.getElementById("body-label");
-const nextBody = document.getElementById("right-body");
-const previousBody = document.getElementById("left-body");
 
 let iBody = 0;
 
-previousBody.addEventListener("click", () => {
+$("left-body").addEventListener("click", () => {
     iBody--;
     if (iBody >= 0) {
-        bodyLabel.innerText = character[iBody].name;
-        chopperBody.style.backgroundImage = `url('${character[iBody].body}')`;
+        $("body-label").innerText = character[iBody].name;
+        $(
+            "chopper-body"
+        ).style.backgroundImage = `url('${character[iBody].body}')`;
     } else {
         iBody = characterAmount - 1;
-        bodyLabel.innerText = character[iBody].name;
-        chopperBody.style.backgroundImage = `url('${character[iBody].body}')`;
+        $("body-label").innerText = character[iBody].name;
+        $(
+            "chopper-body"
+        ).style.backgroundImage = `url('${character[iBody].body}')`;
     }
 });
 
-nextBody.addEventListener("click", () => {
+$("right-body").addEventListener("click", () => {
     iBody++;
     if (iBody < characterAmount) {
-        bodyLabel.innerText = character[iBody].name;
-        chopperBody.style.backgroundImage = `url('${character[iBody].body}')`;
+        $("body-label").innerText = character[iBody].name;
+        $(
+            "chopper-body"
+        ).style.backgroundImage = `url('${character[iBody].body}')`;
     } else {
         iBody = 0;
-        bodyLabel.innerText = character[iBody].name;
-        chopperBody.style.backgroundImage = `url('${character[iBody].body}')`;
+        $("body-label").innerText = character[iBody].name;
+        $(
+            "chopper-body"
+        ).style.backgroundImage = `url('${character[iBody].body}')`;
     }
 });
 
 //MOUTH -------
-const mouthLabel = document.getElementById("mouth-label");
-const nextMouth = document.getElementById("right-mouth");
-const previousMouth = document.getElementById("left-mouth");
-
 let iMouth = 0;
 
-previousMouth.addEventListener("click", () => {
+$("left-mouth").addEventListener("click", () => {
     iMouth--;
     if (iMouth >= 0) {
-        mouthLabel.innerText = character[iMouth].name;
-        chopperMouth.style.backgroundImage = `url('${character[iMouth].mouth}')`;
+        $("mouth-label").innerText = character[iMouth].name;
+        $(
+            "chopper-mouth"
+        ).style.backgroundImage = `url('${character[iMouth].mouth}')`;
     } else {
         iMouth = characterAmount - 1;
-        mouthLabel.innerText = character[iMouth].name;
-        chopperMouth.style.backgroundImage = `url('${character[iMouth].mouth}')`;
+        $("mouth-label").innerText = character[iMouth].name;
+        $(
+            "chopper-mouth"
+        ).style.backgroundImage = `url('${character[iMouth].mouth}')`;
     }
 });
 
-nextMouth.addEventListener("click", () => {
+$("right-mouth").addEventListener("click", () => {
     iMouth++;
     if (iMouth < characterAmount) {
-        mouthLabel.innerText = character[iMouth].name;
-        chopperMouth.style.backgroundImage = `url('${character[iMouth].mouth}')`;
+        $("mouth-label").innerText = character[iMouth].name;
+        $(
+            "chopper-mouth"
+        ).style.backgroundImage = `url('${character[iMouth].mouth}')`;
     } else {
         iMouth = 0;
-        mouthLabel.innerText = character[iMouth].name;
-        chopperMouth.style.backgroundImage = `url('${character[iMouth].mouth}')`;
+        $("mouth-label").innerText = character[iMouth].name;
+        $(
+            "chopper-mouth"
+        ).style.backgroundImage = `url('${character[iMouth].mouth}')`;
     }
 });
 
 //EYES ---------
-const eyesLabel = document.getElementById("eyes-label");
-const nextEyes = document.getElementById("right-eyes");
-const previousEyes = document.getElementById("left-eyes");
 
 let iEyes = 0;
 
-previousEyes.addEventListener("click", () => {
+$("left-eyes").addEventListener("click", () => {
     iEyes--;
     if (iEyes >= 0) {
-        eyesLabel.innerText = character[iEyes].name;
-        chopperEyes.style.backgroundImage = `url('${character[iEyes].eyes}')`;
+        $("eyes-label").innerText = character[iEyes].name;
+        $(
+            "chopper-eyes"
+        ).style.backgroundImage = `url('${character[iEyes].eyes}')`;
     } else {
         iEyes = characterAmount - 1;
-        eyesLabel.innerText = character[iEyes].name;
-        chopperEyes.style.backgroundImage = `url('${character[iEyes].eyes}')`;
+        $("eyes-label").innerText = character[iEyes].name;
+        $(
+            "chopper-eyes"
+        ).style.backgroundImage = `url('${character[iEyes].eyes}')`;
     }
 });
 
-nextEyes.addEventListener("click", () => {
+$("right-eyes").addEventListener("click", () => {
     iEyes++;
     if (iEyes < characterAmount) {
-        eyesLabel.innerText = character[iEyes].name;
-        chopperEyes.style.backgroundImage = `url('${character[iEyes].eyes}')`;
+        $("eyes-label").innerText = character[iEyes].name;
+        $(
+            "chopper-eyes"
+        ).style.backgroundImage = `url('${character[iEyes].eyes}')`;
     } else {
         iEyes = 0;
-        eyesLabel.innerText = character[iEyes].name;
-        chopperEyes.style.backgroundImage = `url('${character[iEyes].eyes}')`;
+        $("eyes-label").innerText = character[iEyes].name;
+        $(
+            "chopper-eyes"
+        ).style.backgroundImage = `url('${character[iEyes].eyes}')`;
     }
 });
 
 //Pants
-const pantsLabel = document.getElementById("pants-label");
-const nextPants = document.getElementById("right-pants");
-const previousPants = document.getElementById("left-pants");
-
 let iPants = 0;
 
-previousPants.addEventListener("click", () => {
+$("left-pants").addEventListener("click", () => {
     iPants--;
     if (iPants >= 0) {
-        pantsLabel.innerText = character[iPants].name;
-        chopperPants.style.backgroundImage = `url('${character[iPants].pants}')`;
+        $("pants-label").innerText = character[iPants].name;
+        $(
+            "chopper-pants"
+        ).style.backgroundImage = `url('${character[iPants].pants}')`;
     } else {
         iPants = characterAmount - 1;
-        pantsLabel.innerText = character[iPants].name;
-        chopperPants.style.backgroundImage = `url('${character[iPants].pants}')`;
+        $("pants-label").innerText = character[iPants].name;
+        $(
+            "chopper-pants"
+        ).style.backgroundImage = `url('${character[iPants].pants}')`;
     }
 });
 
-nextPants.addEventListener("click", () => {
+$("right-pants").addEventListener("click", () => {
     iPants++;
     if (iPants < characterAmount) {
-        pantsLabel.innerText = character[iPants].name;
-        chopperPants.style.backgroundImage = `url('${character[iPants].pants}')`;
+        $("pants-label").innerText = character[iPants].name;
+        $(
+            "chopper-pants"
+        ).style.backgroundImage = `url('${character[iPants].pants}')`;
     } else {
         iPants = 0;
-        pantsLabel.innerText = character[iPants].name;
-        chopperPants.style.backgroundImage = `url('${character[iPants].pants}')`;
+        $("pants-label").innerText = character[iPants].name;
+        $(
+            "chopper-pants"
+        ).style.backgroundImage = `url('${character[iPants].pants}')`;
     }
 });
 
 //Legs
-const legsLabel = document.getElementById("legs-label");
-const nextLegs = document.getElementById("right-legs");
-const previousLegs = document.getElementById("left-legs");
 
 let iLegs = 0;
 
-previousLegs.addEventListener("click", () => {
+$("left-legs").addEventListener("click", () => {
     iLegs--;
     if (iLegs >= 0) {
-        legsLabel.innerText = character[iLegs].name;
-        chopperLegs.style.backgroundImage = `url('${character[iLegs].legs}')`;
+        $("legs-label").innerText = character[iLegs].name;
+        $(
+            "chopper-legs"
+        ).style.backgroundImage = `url('${character[iLegs].legs}')`;
     } else {
         iLegs = characterAmount - 1;
-        legsLabel.innerText = character[iLegs].name;
-        chopperLegs.style.backgroundImage = `url('${character[iLegs].legs}')`;
+        $("legs-label").innerText = character[iLegs].name;
+        $(
+            "chopper-legs"
+        ).style.backgroundImage = `url('${character[iLegs].legs}')`;
     }
 });
 
-nextLegs.addEventListener("click", () => {
+$("right-legs").addEventListener("click", () => {
     iLegs++;
     if (iLegs < characterAmount) {
-        legsLabel.innerText = character[iLegs].name;
-        chopperLegs.style.backgroundImage = `url('${character[iLegs].legs}')`;
+        $("legs-label").innerText = character[iLegs].name;
+        $(
+            "chopper-legs"
+        ).style.backgroundImage = `url('${character[iLegs].legs}')`;
     } else {
         iLegs = 0;
-        legsLabel.innerText = character[iLegs].name;
-        chopperLegs.style.backgroundImage = `url('${character[iLegs].legs}')`;
+        $("legs-label").innerText = character[iLegs].name;
+        $(
+            "chopper-legs"
+        ).style.backgroundImage = `url('${character[iLegs].legs}')`;
     }
 });
 
 //Object
-const objectLabel = document.getElementById("object-label");
-const nextObject = document.getElementById("right-object");
-const previousObject = document.getElementById("left-object");
 
 let iObject = 0;
 
-previousObject.addEventListener("click", () => {
+$("left-object").addEventListener("click", () => {
     iObject--;
     if (iObject >= 0) {
-        objectLabel.innerText = character[iObject].name;
-        chopperObject.style.backgroundImage = `url('${character[iObject].object}')`;
+        $("object-label").innerText = character[iObject].name;
+        $(
+            "chopper-object"
+        ).style.backgroundImage = `url('${character[iObject].object}')`;
     } else {
         iObject = characterAmount - 1;
-        objectLabel.innerText = character[iObject].name;
-        chopperObject.style.backgroundImage = `url('${character[iObject].object}')`;
+        $("object-label").innerText = character[iObject].name;
+        $(
+            "chopper-object"
+        ).style.backgroundImage = `url('${character[iObject].object}')`;
     }
 });
 
-nextObject.addEventListener("click", () => {
+$("right-object").addEventListener("click", () => {
     iObject++;
     if (iObject < characterAmount) {
-        objectLabel.innerText = character[iObject].name;
-        chopperObject.style.backgroundImage = `url('${character[iObject].object}')`;
+        $("object-label").innerText = character[iObject].name;
+        $(
+            "chopper-object"
+        ).style.backgroundImage = `url('${character[iObject].object}')`;
     } else {
         iObject = 0;
-        objectLabel.innerText = character[iObject].name;
-        chopperObject.style.backgroundImage = `url('${character[iObject].object}')`;
+        $("object-label").innerText = character[iObject].name;
+        $(
+            "chopper-object"
+        ).style.backgroundImage = `url('${character[iObject].object}')`;
     }
 });
